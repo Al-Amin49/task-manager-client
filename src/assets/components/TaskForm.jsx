331be8891c06a352299f0a04feba54e6fd9a710c
Al- Amin <alamin496832@gmail.com>
2023-12-12 22:50:04 +0600

    task form created

--- src/assets/components/TaskForm.jsx
+++ src/assets/components/TaskForm.jsx
@@ -1,11 +1,89 @@
 
 
+
 const TaskForm = () => {
     return (
-        <div>
-            <h3 className="text-red-500">Task Form</h3>
+      <div>
+        <div className="bg-green-200 p-4">
+          <form className="w-full max-w-lg">
+            <div className="flex flex-wrap -mx-3 mb-6">
+              <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-first-name"
+                >
+                  Title
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="grid-first-name"
+                  type="text"
+                  placeholder="title"
+                />
+              </div>
+              <div className="w-full md:w-1/2 px-3">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-last-name"
+                >
+                  Description
+                </label>
+                <textarea
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-last-name"
+                  placeholder="Enter task description..."
+                />
+              </div>
+            </div>
+  
+            <div className="flex flex-wrap -mx-3 mb-6">
+            <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+              <label
+                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                htmlFor="status"
+              >
+                Status
+              </label>
+              
+         
+          <select 
+              className=" block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+          >
+            <option value="Not Started">Not Started</option>
+            <option value="In Progress">In Progress</option>
+            <option value="Completed">Completed</option>
+          </select>
+       
+            </div>
+              <div className="w-full md:w-1/2 px-3">
+                <label
+                  className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                  htmlFor="grid-last-name"
+                >
+                  Due Date
+                </label>
+                <input
+                  className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-last-name"
+                  type="date"
+                />
+              </div>
+            </div>
+  
+            
+            <div className="text-center">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-center text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+              >
+                Add Task
+              </button>
+            </div>
+          </form>
         </div>
+      </div>
     );
-};
-
-export default TaskForm;
+  };
+  
+  export default TaskForm;
+  
--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -3,7 +3,8 @@ import TaskForm from "../assets/components/TaskForm";
 
 const TaskPage = () => {
     return (
-        <div>
+        <div className="max-w-sm mx-auto">
+            <h3 className="my-10 text-center">Task To do</h3>
            <TaskForm/>
         </div>
     );
